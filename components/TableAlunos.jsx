499da3c79419e3065fc5c2ff43bb1c62f39a7548
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-10-24 21:41:59 -0300

    AAA

--- components/TableAlunos.jsx
+++ components/TableAlunos.jsx
@@ -1,14 +1,23 @@
 import contem from "@/functions/contem"
 import { PostData, PutData } from "@/pages/api/hello"
 import ModalCadastro from "./ModalCadastro"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import alunosDaTurma from "@/data/alunosDaTurma"
-export default  function TableAlunos({ alunos, turmas, atualizar }) {
+export default function TableAlunos({ alunos, turmas, atualizar }) {
 
     const [mostrarCadastro, setMostrarCadastro] = useState(false);
-
-    //useEffect que define os alunos das turmas
+    // Problemas Aqui
+    useEffect(() => {
+        if (turmas == undefined) return
+        async function getAlunos(){
+            for (let t of turmas) {
+                t.alunos = await alunosDaTurma(t)
+            }
+        }
+        getAlunos()
+        console.log(turmas)
+    }, [turmas, alunos, atualizar])
 
     function post(obj) {
         PostData(obj, "aluno")
@@ -17,22 +26,22 @@ export default  function TableAlunos({ alunos, turmas, atualizar }) {
     function put(aluno, value) {
         aluno.turma = value == "N/A" ? null : { "id": parseInt(value) };
         PutData(aluno, "aluno")
-        atualizar()   
+        atualizar()
     }
     return (
         <div className="flex flex-col gap-1 w-full">
             <div className="titulo">
                 Alunos
-                <button className="botao" onClick={() => setMostrarCadastro(true)}><Image className=" invert" width={16} height={16} alt="adicionar" src="/mais.png"/></button>
-                </div>
+                <button className="botao" onClick={() => setMostrarCadastro(true)}><Image className=" invert" width={16} height={16} alt="adicionar" src="/mais.png" /></button>
+            </div>
             <div className="flex flex-col gap-1">
                 {alunos.map(aluno => {
-                    return <div key={aluno.id}  className="flex gap-1">
+                    return <div key={aluno.id} className="flex gap-1">
                         <div className="linhas w-full">{aluno.nome}</div>
                         <select className="linhas w-min" defaultValue={aluno.turma ? aluno.turma.id : null} onChange={e => put(aluno, e.target.value)}>
                             <option value={null} >N/A</option>
                             {turmas.map(turma => {
-                                if (contem(aluno,  turma.alunos)) {
+                                if (contem(aluno, turma.alunos)) {
                                     return <option key={turma.id} value={turma.id}>{turma.id}</option>
                                 }
                                 return <option key={turma.id} value={turma.id}>{turma.id}</option>
@@ -42,7 +51,7 @@ export default  function TableAlunos({ alunos, turmas, atualizar }) {
                 })}
             </div>
             {mostrarCadastro &&
-                <ModalCadastro post={obj => post(obj)} fechar={() => setMostrarCadastro(false)}/>
+                <ModalCadastro post={obj => post(obj)} fechar={() => setMostrarCadastro(false)} />
             }
         </div>
     )
--- functions/contem.js
+++ functions/contem.js
@@ -1,4 +1,5 @@
 export default (dado, lista) => {
+    console.log(lista)
   for (let l of lista) {
     if (l.id == dado.id) {
       return true;
